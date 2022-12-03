c0f9c61c144c70ed91a5711274c78c176f03895a
Johnbasha-mern <jhonbasha1111@gmail.com>
2022-12-03 22:09:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="container">
+    <div class="box">
+     <div class="textbox">
+        <p>  HTML<span>&</span>CSS</p>
+        <p class="txt">design and build website</p>
+     </div>
+     <div class="square sq-top"></div>
+    <div class="square sq-right"></div>
+    <div class="square sq-bottom"></div>
+    <div class="square sq-left"></div>
+    </div>
+</div>
+  );
+}
+
+export default App;
